7cf8694fdd6ac7f33ed79fe4e2b0f7aac265a112
Guangming Lang <gmlang@cabaceo.com>
2016-09-28 19:27:38 -0400

    correct the mistake when merging

--- ui.R
+++ ui.R
@@ -1,7 +1,7 @@
 navbarPage(title=div(a(href="http://cabaceo.com",
                        img(src="Logo_01.png", id = "logo")), "tsplotr"),
            theme = "darkly1.css", 
-           
+
            tabPanel('Upload Data',
                     sidebarLayout(
                             sidebarPanel(
