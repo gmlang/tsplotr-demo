5ee1c7d96bef311a3ae4e68951256a10c6e5145a
Guangming Lang <gmlang@cabaceo.com>
2016-10-04 14:48:31 -0400

    add progress bar

--- server.R
+++ server.R
@@ -32,29 +32,51 @@ server = function(input, output) {
                 # remove seasonality
                 source(file.path(server_path, "rm-seasonality.R"), local = T)
                 
+                # get residuals and r-squared for lm(the response ~ seasonality)
+                withProgress(message = 'Removing Seasonality...', value = 0.4, {
+                        dat_resid = df_resid()
+                        incProgress(0.2)
+                        Sys.sleep(0.5)
+                        
+                        dat_rsqrd = df_rsqrd()
+                        incProgress(0.2)
+                        Sys.sleep(0.5)
+                        
+                        dat_maxcoef = df_maxcoef()
+                        incProgress(0.2)
+                        Sys.sleep(0.5)
+                })
+                
                 # get user selected vars
                 x = pick_x() 
                 gp = pick_gp()
                 
-                # plot general trend of the residuals after removing seasonality
-                dat_resid = df_resid()
-                output$gg_ts = renderPlot({
-                        # plt_trend is defined in R/helper
-                        p = plt_trend(dat_resid, x, y="residuals", gp)
-                        web_display(p)
-                })
-                
-                # plot r-squared by gp var
-                dat_rsqrd = df_rsqrd()
-                output$gg_rsqrd = renderPlot({
-                        # plt_dotplot is defined in R/helper
-                        plt_dotplot(dat_rsqrd, x="r.squared", gp)
-                })
-                
-                dat_maxcoef = df_maxcoef()
-                output$gg_maxcoef = renderPlot({
-                        # plt_dotplot is defined in R/helper
-                        plt_dotplot(dat_maxcoef, x="max_coef", gp)
+                withProgress(message = 'Plotting ', value = 0.4, {
+                        
+                        # plot general trend of the residuals 
+                        output$gg_ts = renderPlot({
+                                p = plt_trend(dat_resid, x, y="residuals", gp)
+                                web_display(p)
+                        })
+                        
+                        incProgress(0.2, detail = "trend")
+                        Sys.sleep(2)
+                        
+                        # plot r-squared by gp var
+                        output$gg_rsqrd = renderPlot({
+                                plt_dotplot(dat_rsqrd, x="r.squared", gp)
+                        })
+                        
+                        incProgress(0.2, detail = "R-squared")
+                        Sys.sleep(1)
+                        
+                        # plot max coef by gp var
+                        output$gg_maxcoef = renderPlot({
+                                plt_dotplot(dat_maxcoef, x="max_coef", gp)
+                        })
+                        
+                        incProgress(0.2, detail = "Max Coef. Est.")
+                        Sys.sleep(1)
                 })
         })
 }
